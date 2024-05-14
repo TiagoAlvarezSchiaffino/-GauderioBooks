29bd81e0bdf94477c6b79ff63d51eef1f9595f15
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-05-14 15:12:51 -0300

    feat: Add Font Awesome

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping , faUser} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
@@ -6,10 +8,10 @@ const Navbar = () => {
         <h1 className=" text-3xl font-bold uppercase p-5">Gauderio Libros</h1>
         <ul className="flex items-center justify-end">
           <li className="uppercase p-3">inicio</li>
-          <li className="uppercase p-3">catalogo</li>
-          <li className="uppercase p-3">compras online</li>
-          <li className="uppercase p-3">login</li>
-          <li className="uppercase p-3">Cartwidget</li>
+          <li className="uppercase p-4">catalogo</li>
+          <li className="uppercase p-4">compras online</li>
+          <li className="u p-5"><FontAwesomeIcon icon={faUser} /></li>    {/*-----login----*/}
+          <li className=" p-9"><FontAwesomeIcon icon={faCartShopping} /></li>  {/*-----CARRITO DE COMPRA----*/}
         </ul>
       </nav>
   )

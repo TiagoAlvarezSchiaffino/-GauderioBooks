7003d202d2644b34872f640094a2527eb712912e
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-05-18 20:20:53 -0300

    feat: Add Presentation

---
+++ client/src/components/pages/Presentation.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Presentation = () => {
+  return (
+    <div className="bg-white p-16">
+      <div className="bg-black rounded-lg flex justify-center items-center px-5">
+        <div className="text-white p-5 w-1/2">
+          <h1 className="text-3xl text-cyan-200 p-9 pt-1"></h1>
+          <p className="p-2 w-3/4 pl-8">
+         </p> 
+         <p className="p-2 w-3/4 pl-8">
+          </p >
+          <p className="p-2 w-3/4 pl-8"></p>
+        </div>
+        <div className="text-white p-5 w-1/2 rounded">
+          <img />
+        </div>
+      </div>
+      <div className="flex justify-between mt-10 w-full">
+        <div className="w-1/2 ">
+          <img />
+        </div>
+        <div className="w-1/2 ">
+          <img />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Presentation;
